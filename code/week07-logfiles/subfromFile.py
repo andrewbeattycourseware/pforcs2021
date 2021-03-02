9167ea3e07c735caeece8de501e6e8cdbcf272aa
Andrew Beatty <andrew.beatty@gmit.ie>
2021-03-02 16:55:46 +0000

    week07

---
+++ code/week07-logfiles/subfromFile.py
@@ -0,0 +1,19 @@
+# This is code anonymise the sub domains of ip addresses 
+# Author: Andrew Beatty
+
+import re
+
+regex = "\d{1,3}\.\d{1,3} " # note the space at the end
+replacementText="XXX.XXX " # note the space at the end to match above
+filename = "./sample-files/smallerAccess.log"
+outputFileName = "anonymisedIPs.txt"
+
+with open(filename) as inputFile:
+    with open(outputFileName, 'w') as outputFile:
+        for line in inputFile:
+            # for debugging
+            #foundText = re.search(regex, line).group()
+            #print(foundText)
+            newLine = re.sub(regex, replacementText, line)
+            outputFile.write(newLine)
+            
